8730e409ccb042c4f5a7362dd3d750965a40eb05
toddmac12 <toddmac12@gmail.com>
2021-10-18 17:31:46 -0400

    folders and files added to project

---
+++ RecipeBox/Models/Recipe.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace RecipeBox.Models
+{
+  public class Recipe
+  {
+    public Recipe()
+    {
+      this.JoinEntities = new HashSet<CategoryRecipe>();
+    }
+
+    public int RecipeId { get; set; }
+    public string Name { get; set; }
+    public string Ingredient { get; set; }
+    public string Instruction { get; set; }
+    public virtual ApplicationUser User { get; set; }
+
+    public virtual ICollection<CategoryRecipe> JoinEntities { get; }
+  }
+}
---
+++ RecipeBox/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RecipeBox.Models;
+//new code
+using Microsoft.AspNetCore.Identity;
+
+namespace RecipeBox
+{
+  public class Startup
+  {
+    public Startup(IWebHostEnvironment env)
+    {
+      var builder = new ConfigurationBuilder()
+          .SetBasePath(env.ContentRootPath)
+          .AddJsonFile("appsettings.json");
+      Configuration = builder.Build();
+    }
+
+    public IConfigurationRoot Configuration { get; set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+
+      services.AddEntityFrameworkMySql()
+        .AddDbContext<RecipeBoxContext>(options => options
+        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+
+      //new code
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<RecipeBoxContext>()
+                .AddDefaultTokenProviders();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseDeveloperExceptionPage();
+
+      //new code
+      app.UseAuthentication(); 
+
+      app.UseRouting();
+
+      //new code
+      app.UseAuthorization();
+
+      app.UseEndpoints(routes =>
+      {
+        routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+      });
+
+      app.UseStaticFiles();
+
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("Hello World!");
+      });
+    }
+  }
+}
